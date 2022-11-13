012e13ace980fbe3adc88cb63da522e30d26d253
seckinyagci <seckininadresi@gmail.com>
2022-11-13 20:58:43 +0100

    oca

--- src/main/java/OCA_Sorulari_1/DataCasting.java
+++ src/main/java/OCA_Sorulari_1/DataCasting.java
@@ -1,4 +1,16 @@
 package OCA_Sorulari_1;
 
 public class DataCasting {
+    public static void main(String[] args) {
+        /*
+        lon x=10;
+        int y= 2* x;
+        hangi
+         */
+        byte a=120;
+        byte b=50;
+        byte sum = (byte) (a+b);
+        System.out.println(sum);
+
+    }
 }
---
+++ src/main/java/OCA_Sorulari_1/StringManipulationOca.java
@@ -0,0 +1,14 @@
+package OCA_Sorulari_1;
+
+public class StringManipulationOca {
+    public static void main(String[] args) {
+        String number= "012345678";
+
+        System.out.println(number.substring(1,3));
+        System.out.println(number.substring(7,7));
+
+    }
+    public void methodA(){
+
+    return ;} //void methoda bos return yaptirabiliyorsunuz
+}
